8b936e97b06639eb9f38d2f4911ba27089a3194d
KishanSadeesh <131572971+KishanSadeesh@users.noreply.github.com>
2024-06-08 11:07:24 +0530

    dial activity

---
+++ app/src/main/java/com/example/myappkotlin/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.myappkotlin
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun btnlogcat(view: View) {
+        Log.i("MainActivity", "Button clicked")
+        var dial: Intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:9047478386"))
+        startActivity(dial)
+    }
+}
